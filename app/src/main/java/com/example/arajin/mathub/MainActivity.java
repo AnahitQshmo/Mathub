258d64c14417ae755658f2326934928b7098d0d3
AnahitQshmo <anahitqishmiryan49@gmail.com>
2024-03-02 12:01:22 +0400

    first commit

---
+++ app/src/main/java/com/example/arajin/mathub/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.arajin.mathub;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView
+        .OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView
+                = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.n_home);
+    }
+    HomeFragment homeFragment = new HomeFragment();
+    SchoolFragment schoolFragment = new SchoolFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.n_home) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, homeFragment)
+                    .commit();
+            return true;
+        } else if (itemId == R.id.n_profile) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, profileFragment)
+                    .commit();
+            return true;
+        } else if (itemId == R.id.n_school) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, schoolFragment)
+                    .commit();
+            return true;
+        }
+
+        return false;
+    }
+
+}
